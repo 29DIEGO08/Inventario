15817962aae6c24db40074f71229f0749f39faae
29DIEGO08 <programacio2905@gmail.com>
2024-11-09 12:24:47 -0500

    linkpaginaprincipal

--- js/inicio.js
+++ js/inicio.js
@@ -3,7 +3,7 @@ function navigate() {
     if (userType === "Propietario") {
         window.location.href = "controlPro.html";
     } else if (userType === "Cliente") {
-        window.location.href = "cliente.html";
+        window.location.href = "sesion.html";
     } else {
         alert("Por favor, seleccione un tipo de usuario.");
     }

b7e2ac92ee414f7841f97762db4d4e10fefea394
Camilo9807 <galvisdunier@gmail.com>
2024-11-16 10:25:41 -0500

    c2

--- js/inicio.js
+++ js/inicio.js
@@ -1,7 +1,7 @@
 function navigate() {
     var userType = document.getElementById("userType").value;
     if (userType === "Propietario") {
-        window.location.href = "controlPro.html";
+        window.location.href = "sesion2.html";
     } else if (userType === "Cliente") {
         window.location.href = "sesion.html";
     } else {
